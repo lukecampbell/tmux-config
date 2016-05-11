123e8d8ac2c5d62ca3ae15fc716adcb38b2dcca0
Luke Campbell <luke.s.campbell@gmail.com>
2016-05-11 09:23:52 -0400

    Uses tpm and tmux-yank instead of os-x script

--- install.sh
+++ install.sh
@@ -2,4 +2,9 @@
 pushd $HOME
 git clone https://github.com/lukecampbell/tmux-config.git $HOME/.tmux
 ln -s $HOME/.tmux/.tmux.conf $HOME/.tmux.conf
+pushd $HOME/.tmux
+git submodule update
 popd
+popd
+
+echo "Be sure to install the plugins on your first tmux session with Ctrl+a then I"
